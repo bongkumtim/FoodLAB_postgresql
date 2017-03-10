ec854df15ef4ae77803897dd06612a641a50af36
Bong <mjbond_bong2@yahoo.com>
2017-03-10 11:19:38 +0800

    create recipe/routes/pages- index/show/new/create/update/edit/destroy"

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,53 @@
+class RecipesController < ApplicationController
+	before_action :find, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@recipes = Recipe.all.order(created_at: :desc)
+	end
+
+	def show
+		
+	end
+
+	def new
+		@recipe = Recipe.new
+	end
+
+	def create
+		@recipe = Recipe.new(recipe_params)
+
+		if @recipe.save
+			redirect_to @recipe, notice: "Successfully created a new recipe"
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @recipe.update(recipe_params)
+		   redirect_to @recipe
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		if @recipe.destroy
+		   redirect_to root_path
+		end
+	end
+
+
+	private
+
+	def recipe_params
+		params.require(:recipe).permit(:title, :description)
+	end
+
+	def find
+		@recipe = Recipe.find(params[:id])
+	end
+end
